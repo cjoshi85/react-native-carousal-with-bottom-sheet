bda2d876e90f54e1bf44ebb770ad33656e3f1594
Chetan Joshi <chetan.joshi@medtrail.in>
2021-01-10 16:25:52 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,108 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import BottomSheet, { BottomSheetScrollView } from "@gorhom/bottom-sheet";
+import { StyleSheet, Animated, Text, View, Dimensions, Image, FlatList } from 'react-native';
 
+const {width, height} = Dimensions.get('screen');
+
+const ITEM_WIDTH = width;
+const ITEM_HEIGHT = height * .75;
+
+const images = [
+  'https://static.zara.net/photos///2020/I/1/1/p/6543/610/091/2/w/2460/6543610091_1_1_1.jpg?ts=1606727905128',
+  'https://static.zara.net/photos///2020/I/1/1/p/6543/610/091/2/w/2460/6543610091_2_1_1.jpg?ts=1606727908993',
+  'https://static.zara.net/photos///2020/I/1/1/p/6543/610/091/2/w/2460/6543610091_2_2_1.jpg?ts=1606727889015',
+  'https://static.zara.net/photos///2020/I/1/1/p/6543/610/091/2/w/2460/6543610091_2_3_1.jpg?ts=1606727896369',
+  'https://static.zara.net/photos///2020/I/1/1/p/6543/610/091/2/w/2460/6543610091_2_4_1.jpg?ts=1606727898445',
+];
+
+const product = {
+  title: 'SOFT MINI CROSSBODY BAG WITH KISS LOCK',
+  description: [
+    'Mini crossbody bag available in various colours. Featuring two compartments. Handles and detachable crossbody shoulder strap. Lined interior. Clasp with two metal pieces.',
+    'Height x Length x Width: 14 x 21.5 x 4.5 cm. / 5.5 x 8.4 x 1.7"'
+  ],
+  price: '29.99£'
+};
+
+const DOT_SIZE = 8;
+const DOT_SPACING = 8;
+const DOT_INDICATOR_SIZE = DOT_SIZE + DOT_SPACING;
 export default function App() {
+  const scrollY = React.useRef(new Animated.Value(0)).current;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1}}>
+      <StatusBar hidden/>
+      <View style={{height: ITEM_HEIGHT, overflow: 'hidden'}}>
+        <Animated.FlatList
+          data={images}
+          keyExtractor={(_, index) => index.toString()}
+          snapToInterval={ITEM_HEIGHT}
+          decelerationRate="fast"
+          onScroll={Animated.event(
+            [{nativeEvent: {contentOffset: {y: scrollY}}}],
+            {useNativeDriver: true}
+          )}
+          renderItem={({item}) => {
+            return (
+              <View>
+                <Image source={{uri: item}} style={styles.image}/>
+              </View>
+            )
+          }}
+          showsVerticalScrollIndicator={false}
+        />
+        <View style={styles.pagination}>
+          {images.map((_, index) => {
+            return (
+              <View key={index} style={styles.dot}/>
+            )
+          })}
+          <Animated.View style={[styles.dotIndicator, {
+            transform: [{
+              translateY: Animated.divide(scrollY, ITEM_HEIGHT).interpolate({
+                inputRange: [0, 1],
+                outputRange: [0, DOT_INDICATOR_SIZE]
+              })
+            }]
+          }]}/>
+        </View>
+      </View>
+      <BottomSheet
+        snapPoints={[height - ITEM_HEIGHT, height]}
+      >
+        <Text style={{fontSize: 17, fontWeight: 'bold', textTransform: 'uppercase',paddingHorizontal:20,paddingBottom:10}}>{product.title}</Text>
+        <BottomSheetScrollView contentContainerStyle={{paddingHorizontal: 20}}>
+          <Text style={{fontSize: 17}}>{product.price}</Text>
+          {product.description.map((item, index) => (
+            <Text key={index} style={{fontSize: 16, marginBottom: 20, lineHeight: 22}}>{item}</Text>
+          ))}
+          <Text style={{fontSize: 17}}>{product.price}</Text>
+          {product.description.map((item, index) => (
+            <Text key={index} style={{fontSize: 16, marginBottom: 20, lineHeight: 22}}>{item}</Text>
+          ))}
+          <Text style={{fontSize: 17}}>{product.price}</Text>
+          {product.description.map((item, index) => (
+            <Text key={index} style={{fontSize: 16, marginBottom: 20, lineHeight: 22}}>{item}</Text>
+          ))}
+          <Text style={{fontSize: 17}}>{product.price}</Text>
+          {product.description.map((item, index) => (
+            <Text key={index} style={{fontSize: 16, marginBottom: 20, lineHeight: 22}}>{item}</Text>
+          ))}
+          <Text style={{fontSize: 17}}>{product.price}</Text>
+          {product.description.map((item, index) => (
+            <Text key={index} style={{fontSize: 16, marginBottom: 20, lineHeight: 22}}>{item}</Text>
+          ))}
+          <Text style={{fontSize: 17}}>{product.price}</Text>
+          {product.description.map((item, index) => (
+            <Text key={index} style={{fontSize: 16, marginBottom: 20, lineHeight: 22}}>{item}</Text>
+          ))}
+          <Text style={{fontSize: 17}}>{product.price}</Text>
+          {product.description.map((item, index) => (
+            <Text key={index} style={{fontSize: 16, marginBottom: 20, lineHeight: 22}}>{item}</Text>
+          ))}
+        </BottomSheetScrollView>
+      </BottomSheet>
     </View>
   );
 }
@@ -18,4 +114,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    height: ITEM_HEIGHT,
+    width: ITEM_WIDTH,
+    resizeMode: 'cover'
+  },
+  pagination: {
+    position: 'absolute',
+    top: ITEM_HEIGHT / 2,
+    left: 20
+  },
+  dot: {
+    height: DOT_SIZE,
+    width: DOT_SIZE,
+    borderRadius: DOT_SIZE,
+    backgroundColor: '#333',
+    marginBottom: DOT_SPACING
+  },
+  dotIndicator: {
+    height: DOT_INDICATOR_SIZE,
+    width: DOT_INDICATOR_SIZE,
+    borderRadius: DOT_INDICATOR_SIZE,
+    borderWidth: 1,
+    borderColor: '#333',
+    position: 'absolute',
+    top: -DOT_SPACING / 2,
+    left: -DOT_SPACING / 2
+  }
 });
---
+++ README.md
@@ -0,0 +1,4 @@
+# react-native-carousal-with-scrollable-bottom-sheet
+**React native Animated component to create carousal like E-commerce app with images and content in bottom sheet.**
+
+![](carousal-with-bottom-sheet.gif)
